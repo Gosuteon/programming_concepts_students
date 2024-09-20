6c6def65b77e39f2a90bd683a09828b5578edb9f
Matteo <matteo.federico@student.uclouvain.be>
2024-09-20 11:49:03 +0200

    resolution CommonElements

--- src/main/java/basics/CommonElements.java
+++ src/main/java/basics/CommonElements.java
@@ -10,7 +10,20 @@ public class CommonElements {
      *         for instance count([1,3,5,5],[1,2,5,5,6]) = 3
      */
     public static int count(int [] tab1, int [] tab2) {
-         return -1;
+        int count = 0;
+        int taille = 0;
+        if (tab1.length>tab2.length){
+            taille = tab2.length;
+        }
+        else {
+            taille = tab1.length;
+        }
+        for (int i = 0; i<taille;i++){
+            if(tab1[i]==tab2[i]){
+                count +=1;
+            }
+        }
+        return count;
     }
 
     /**
@@ -21,6 +34,28 @@ public class CommonElements {
      *         more exactly the size of set {(i,j) such that tab1[i][j] == tab2[i][j]}
      */
     public static int count(int [][] tab1, int [][] tab2) {
-         return -1;
+        int count = 0;
+        int taille = 0;
+        int taille2 = 0;
+        if (tab1.length>tab2.length){
+            taille = tab2.length;
+        }
+        else {
+            taille = tab1.length;
+        }
+        for (int i = 0; i<taille;i++){
+            if (tab1[i].length>tab2[i].length){
+                taille2 = tab2[i].length;
+            }
+            else {
+                taille2 = tab1[i].length;
+            }
+            for(int j = 0; j<taille2;j++){
+                if (tab1[i][j] == tab2[i][j]) {
+                    count += 1;
+                }
+            }
+        }
+        return count;
     }
 }
