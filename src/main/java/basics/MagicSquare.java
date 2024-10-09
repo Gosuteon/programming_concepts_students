5cb4e9d6674e3a95e5c181174829aab6cf68b5c9
Matteo <matteo.federico@student.uclouvain.be>
2024-10-09 14:49:15 +0200

    resolution MagicSquare

--- src/main/java/basics/MagicSquare.java
+++ src/main/java/basics/MagicSquare.java
@@ -1,5 +1,7 @@
 package basics;
 
+import java.util.ArrayList;
+
 public class MagicSquare {
 
 
@@ -24,7 +26,80 @@ public class MagicSquare {
      * @return true if matrix is a n x n magic square, false otherwise
      */
     public static boolean isMagicSquare(int [][] matrix) {
-        // TODO Implement the body of this method, feel free to add other methods but do not change the signature of isMagiSquare
-         return false;
+        int l = matrix.length;
+        int[][] tempo = matrix;
+        int sum = 0;
+        for (int i = 0; i<l;i++){
+            sum += matrix[i][i];
+        }
+        // check range
+
+        for(int i = 0; i<l; i++){
+            for(int j =0; j<l; j++){
+                if(matrix[i][j]==0 || matrix[i][j] > l*l){
+                    return false;
+                }
+            }
+        }
+        // test if square
+        if (matrix.length != matrix[0].length){
+            return false;
+        }
+
+        // check num diff
+        for (int i = 0; i<l;i++){
+            for (int j = 0; j<l;j++){
+                for (int k = 0; k<l; k++){
+                    for (int m = 0; m<l; m++){
+                        if(i!=k && j!=m && matrix[i][j]==tempo[k][m]){
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+
+        // sum ligne
+        for (int i= 0; i<l;i++){
+            int count = 0;
+            for (int j=0; j<l;j++){
+                count += matrix[i][j];
+            }
+            if (count != sum){
+                return false;
+            }
+
+
+        }
+        // sum colonne
+        for (int i= 0; i<l;i++){
+            int count = 0;
+            for (int j=0; j<l;j++){
+                count += matrix[j][i];
+            }
+            if (count != sum){
+                return false;
+            }
+
+
+        }
+        // sum diagonal 1
+        int count = 0;
+        for (int i = 0; i<l;i++){
+            count += matrix[i][i];
+        }
+
+        if (count != sum){
+            return false;
+        }
+
+        // sum diagonal 2
+        count = 0;
+        for (int i = 0; i<l; i++){
+            count += matrix[i][l-1-i];
+        }
+        return count == sum;
+
+
     }
 }
